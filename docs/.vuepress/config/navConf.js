a1b64f0c1c8a692f27782d1d366adc661a47b609
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-06-28 10:44:12 +0800

    路径调整

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,8 +1,9 @@
 module.exports = [
   { text: '首页', link: '/' },
   { text: '高频考点', link:'/高频考点/'},
-  { text:"标签云", link:'/tags/', tags:true },
+  // { text:"标签云", link:'/tags/', tags:true },
   { text: '施工技术', items: [
+    { text: '施工技术', link: '/2.施工技术/设备安装工程' },
     { text: '线路工程通用施工技术', link: '/2.施工技术/5.线路工程/1.通用技术/1.路由复测' },
   ]},
   { text: '准备', link:'/3.施工管理/1.施工准备/1.技术准备'},
