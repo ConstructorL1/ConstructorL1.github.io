18f6e3096c32aa1e8cae7cc9260c828e5ac745ca
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-06-28 21:55:25 +0800

    追加施工安全管理

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -2,6 +2,9 @@ module.exports = {
   '@vuepress/back-to-top': true,
   "vuepress-plugin-auto-sidebar": {
     // refer：https://github.com/shanyuhai123/vuepress-plugin-auto-sidebar/blob/master/lib/options.js
-    sidebarDepth: 2,
+    sidebarDepth: 3,
+    collapseList: [
+      "/3.施工管理/"
+    ]
   }
 };
