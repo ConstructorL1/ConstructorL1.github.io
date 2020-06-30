4b442c71a915c029f2b0d37e9e92767f149b02b3
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-06-30 20:11:27 +0800

    追加质量管理内容

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -1,5 +1,4 @@
 module.exports = {
-  '@vuepress/back-to-top': true,
   "vuepress-plugin-auto-sidebar": {
     // refer：https://github.com/shanyuhai123/vuepress-plugin-auto-sidebar/blob/master/lib/options.js
     sidebarDepth: 3,
