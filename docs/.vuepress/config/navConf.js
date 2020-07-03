9b752ffcd1529298ac4f934406c715092bc27e36
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-07-03 09:08:04 +0800

    更新链接文字

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -6,6 +6,6 @@ module.exports = [
     { text: '线路工程通用施工技术', link: '/2.施工技术/5.线路工程/1.通用技术/1.路由复测' },
   ]},
   { text: '施工管理', link:'/3.施工管理/'},
-  { text: '链接', link:'/link/'},
+  { text: '链接', link:'/link/Old-Yang-001'},
   { text: '历年真题', link:'/5.历年真题/'},
 ];
