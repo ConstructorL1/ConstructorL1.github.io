22869a235e1840a55724c75185be2c5b8a5dd3d5
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-07-07 17:51:33 +0800

    追加法规1Z306000安全生产法律制度

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,6 +1,7 @@
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '高频考点', link:'/高频考点/'},
+  { text: '法规', link: '/IZ300000法规/' },
+  // { text: '高频考点', link:'/高频考点/'},
   { text: '施工技术', link: '/IL412000施工技术/' },
   { text: '施工管理', link:'/IL420000施工管理/'},
   { text: '链接', link:'/link/Old-Yang-001'},
