db484f06b14c7342ad37b4cd0437622f2b02f8f5
曾银田 <zengyintian@cmiot.chinamobile.com>
2020-07-10 10:27:07 +0800

    文件夹重命名

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -1,9 +1,10 @@
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '法规', link: '/IZ300000法规/' },
+  { text: '法规', link: '/1Z300000法规/' },
+  { text: '经济', link: '/1Z100000经济/' },
   // { text: '高频考点', link:'/高频考点/'},
-  { text: '施工技术', link: '/IL412000施工技术/' },
-  { text: '施工管理', link:'/IL420000施工管理/'},
+  { text: '施工技术', link: '/1L412000施工技术/' },
+  { text: '施工管理', link:'/1L420000施工管理/'},
   { text: '链接', link:'/link/Old-Yang-001'},
   { text: '历年真题', link:'/5.历年真题/'},
 ];
